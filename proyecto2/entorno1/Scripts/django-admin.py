3513995b04c02374b9ef7f6e7c51a4da0de87cd1
adiazc50 <30503952+adiazc50@users.noreply.github.com>
2020-04-15 19:08:27 -0500

    Proyecto creado con DJango

---
+++ proyecto2/entorno1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alejan~1\desktop\proyecto\proyec~1\entorno1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
